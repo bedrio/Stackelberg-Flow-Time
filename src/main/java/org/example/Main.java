207e3ab4a8de1cc0dc249d83477a16ffdb2b3051
Bader Alrifai <bedrrifai1@gmail.com>
2023-04-30 20:34:39 -0700

    set visualization as true

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        boolean visualize = false;
+        boolean visualize = true;
         boolean simpleDebug = false;
         int numberOfPlayers = 500;
         int numberOfEpochs = 100000;
